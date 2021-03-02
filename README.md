83c0011d53081fe8b92ca7bce82d0e3259b8e8bf
Jeffery Patterson <jpatterson933@ucla.edu>
2021-03-02 14:07:38 -0800

    added console logs for weather data needed

--- README.md
+++ README.md
@@ -3,16 +3,16 @@
 ## Assignment Requirements:
 GIVEN a weather dashboard with form inputs
 
-<!-- WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history -->
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
 
 Here I will need a search bar input - that input will need to be pushed onto the end of our url string. 
 
 Once the specific city is found, I will need it to pull that five day forcast and we will need to log that city in our local storage nad have it presented somewhere on the screen.
 
 
-<!-- WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index -->
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 
 WHEN I view the UV index
 THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
--- assets/js/script.js
+++ assets/js/script.js
@@ -22,8 +22,13 @@ $("#button-search").on("click", function (event) {
         })
         .then(function (data) {
 
-            //console log information so we can see whats going on
-            console.log(data)
+            //console log our city name so we know where to find it
+            //this can be used as the title plug for our cards
+            console.log(data.city.name)
+            //store city name
+            localStorage.setItem("cityName", data.city.name)
+
+            // console log our latitude and longtitude so we know where to find them
             console.log(data.city.coord.lat);
             console.log(data.city.coord.lon);
 
@@ -43,26 +48,66 @@ $("#button-search").on("click", function (event) {
 
                     //console.log our results from the new url so we can the further refine our weather program
                     console.log(results)
+                    //these are the results for the current day which are kept sepearte form the 
+                    //daily results, so we will need to console log and store current weather conditions
+                    //we could even have an on click even that stores it on the page until they change it
+                    console.log("current day")
+                    console.log(results.current.dt)
+                    localStorage.setItem("currentDate", results.current.dt)
+                    console.log(results.current.temp)
+                    localStorage.setItem("currentTemp", results.current.temp)
+                    console.log(results.current.wind_speed)
+                    console.log(results.current.humidity)
+                    console.log(results.current.uvi)
+                    console.log(results.current.weather[0].main)
+                    console.log(results.current.weather[0].icon)
+                    console.log(results.current.sunset)
+                    console.log(results.current.sunrise)
+                    console.log("end current day info")
+                    
 
+                    //loops through our daily weather reports for the next five days
+                    for (var i = 1; i < 6; i++) {
 
+                        var maximumTemp = results.daily[i].temp.max
+                        var minimumTemp = results.daily[i].temp.min
+                        var avgTemp = (maximumTemp + minimumTemp) / 2
+
+                        //takes the average of maximum and minimum temperatures
+                        console.log("new day")
+                        console.log(avgTemp)
+                        //console log our date
+                        console.log(results.daily[i].dt)
+                        //console log our max temp
+                        console.log(results.daily[i].temp.max)
+                        //console log our min temp
+                        console.log(results.daily[i].temp.min)
+                        //console log our humidity
+                        console.log(results.daily[i].wind_speed)
+                        //console log our humidity - this can be translated to percentage to represent total humidty in the air. (eg. 49 means humidity is 49%)
+                        console.log(results.daily[i].humidity)
+                        //console log our uvi
+                            //UV Index 0 - 2. Low exposure level. Average time it takes to burn: 60 minutes. ...
+                            // UV Index 3 - 5. Moderate exposure level. Average time it takes to burn: 45 minutes. ...
+                            // UV Index 6 - 7. High exposure level. ...
+                            // UV Index 8 - 10. Very high exposure level. ...
+                            // 11+ UV Index. Extreme exposure level.
+                        //I will need to create  uvi function
+                        console.log(results.daily[i].uvi)
+                        //console log weather conditions report string
+                        //i could go further and loop throug the weather array or just set it to 0
+                        console.log(results.daily[i].weather[0].id)
+                        console.log(results.daily[i].weather[0].main)
+                        console.log(results.daily[i].weather[0].description)
+                        console.log(results.daily[i].weather[0].icon)
+                        //console log our sunrise time
+                        console.log(results.daily[i].sunrise)
+                        //console log our sunet time
+                        console.log(results.daily[i].sunset) 
+                    }
                 })
-
-
-            // this for loop can be used when you run it through the daily forcast api
-            // for (var i = 0; i < data.list.length; i++) {
-            //     console.log(data.list[i].dt_txt);
-            //     console.log(data.list[i].main.temp);
-            //     console.log(data.list[i].main.humidity);
-            //     console.log(data.list[i].wind.speed);
-
-            //     console.log(data.list[i].main.feels_like);
-    
-    
-            // }
-    
         })
 })
-
 
 
 
