6e753ee915c5d5c6942ded2a9db362c0f349dabd
Jeffery Patterson <jpatterson933@ucla.edu>
2021-03-02 19:50:43 -0800

    created function to display current daysforecast based off of city input

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@ var forecastUrl = "https://api.openweathermap.org/data/2.5/onecall?";
 var apiKey = "&appid=3eba9a255d0b187b6983dc669df8b195"
 
 
+
+
 //when the user clicks this button, it will run the events for the pages
 $("#button-search").on("click", function (event) {
     event.preventDefault();
@@ -99,54 +101,131 @@ $("#button-search").on("click", function (event) {
                     //loops through our daily weather reports for the next five days
                     for (var i = 1; i < 6; i++) {
                         
-                        //console log to track each new day through loops
-                        console.log("new day")
-                        //console log our date
-                        console.log(results.daily[i].dt)
-                        
                         //date string
                         dateStr += results.daily[i].dt + ", ";
-                        console.log(dateStr)
                         localStorage.setItem("dailyDate", dateStr)
-                     
-                        //console log our max temp
-                        console.log(results.daily[i].temp.max)
 
-                        //max temp string
+                        //temp max string
                         tempMaxStr += results.daily[i].temp.max + ", ";
                         localStorage.setItem("dailyTempMax", tempMaxStr)
-
-                        //console log our min temp
-                        console.log(results.daily[i].temp.min)
 
                         //temp min string
                         tempMinStr += results.daily[i].temp.min + ", ";
                         localStorage.setItem("dailyTempMin", tempMinStr)
 
-                        //console log our humidity
-                        console.log(results.daily[i].wind_speed)
-
+                        //wind speed string
                         windSpeedStr += results.daily[i].wind_speed + ", ";
                         localStorage.setItem("dailyWindSpeed", windSpeedStr)
-                        //console log our humidity - this can be translated to percentage to represent total humidty in the air. (eg. 49 means humidity is 49%)
-                        console.log(results.daily[i].humidity)
-                        //console log our uvi
-                        //I will need to create  uvi function
-                        console.log(results.daily[i].uvi)
-                        //console log weather conditions report string
-                        //i could go further and loop throug the weather array or just set it to 0
-                        console.log(results.daily[i].weather[0].main)
-                        console.log(results.daily[i].weather[0].icon)
-                        //console log our sunrise time
-                        console.log(results.daily[i].sunrise)
-                        //console log our sunet time
-                        console.log(results.daily[i].sunset) 
+
+                        //humidity string
+                        humidityStr += results.daily[i].humidity + ", ";
+                        localStorage.setItem("dailyHumidity", humidityStr)
+
+                        //uvi string
+                        uviStr += results.daily[i].uvi + ", ";
+                        localStorage.setItem("dailyUvi", uviStr);
+
+                        //weather condition main string
+                        weatherConditionMainStr += results.daily[i].weather[0].main + ", ";
+                        localStorage.setItem("dailyConditionMain", weatherConditionMainStr)
+
+                        //weather condition icon string
+                        weatherConditionIconStr += results.daily[i].weather[0].icon + ", ";
+                        localStorage.setItem("dailyConditionImg", weatherConditionIconStr)
+
+                        //sunrise string
+                        sunriseStr += results.daily[i].sunrise + ", ";
+                        localStorage.setItem("dailySunrise", sunriseStr);
+
+                        //sunset string
+                        sunsetStr += results.daily[i].sunset + ", ";
+                        localStorage.setItem("dailySunset", sunsetStr)
                     }
                 })
         })
 })
 
+var dateStr = localStorage.getItem("dailyDate").split(",");
+var tempMaxStr = localStorage.getItem("dailyTempMax").split(",");
+var tempMinStr = localStorage.getItem("dailyTempMin").split(",");
+var windSpeedStr = localStorage.getItem("dailyWindSpeed").split(",");
+var humidityStr = localStorage.getItem("dailyHumidity").split(",");
+var uviStr = localStorage.getItem("dailyUvi").split(",");
+var weatherConditionMainStr = localStorage.getItem("dailyConditionMain").split(",");
+var weatherConditionIconStr = localStorage.getItem("dailyConditionImg").split(",");
+var sunriseStr = localStorage.getItem("dailySunrise").split(",");
+var sunsetStr = localStorage.getItem("dailySunset").split(",");
 
+// console.log(dateStr)
+// console.log(tempMaxStr)
+// console.log(tempMinStr)
+// console.log(windSpeedStr)
+// console.log(humidityStr)
+// console.log(uviStr)
+// console.log(weatherConditionMainStr)
+// console.log(weatherConditionIconStr)
+// console.log(sunriseStr)
+// console.log(sunsetStr)
+
+
+//variables for displaying card information. City Name remains global.
+var currentDayDisplay = $("#current-day");
+var cityNameGlobal =  localStorage.getItem("cityName");
+
+
+
+
+
+function currentDay () {
+
+    currentDayForecast = {
+        cityName: localStorage.getItem("cityName"),
+        date: localStorage.getItem("currentDate").trim(),
+        temp: localStorage.getItem("currentTemp").trim(),
+        windSpeed: localStorage.getItem("currentWindSpeed").trim(),
+        humidity: localStorage.getItem("currentHumidity").trim(),
+        uvi: localStorage.getItem("currentUvi").trim(),
+        conditions: localStorage.getItem("currentConditions").trim(),
+        conditionImg: localStorage.getItem("currentConditionsImg").trim(),
+        sunset: localStorage.getItem("currentSunset").trim(),
+        sunrsie: localStorage.getItem("currentSunrise").trim()
+    }
+
+    var displayColumn1 = $("<div class='col-sm-3'></div>");
+    var displayCity = $("<div class='row'></div>");
+    var displayDate = $("<div class='row'><p></p></div>");
+    var displayTemp = $("<div class='row'></div>");
+    var displayWind = $("<div class='row'></div>"); 
+    var displayHumidity = $("<div class='row'></div>");
+    var displayUvi = $("<div class='row'></div>");
+    var displayConditions = $("<div class='row'></div>");
+
+    var displayCity = $("<h1>" + currentDayForecast.cityName + "</h1>")
+
+    displayCity.append(displayCity)
+    displayDate.append(currentDayForecast.date)
+    displayTemp.append(currentDayForecast.temp)
+    displayWind.append(currentDayForecast.windSpeed)
+    displayHumidity.append(currentDayForecast.humidity)
+    displayUvi.append(currentDayForecast.uvi)
+    displayConditions.append(currentDayForecast.conditions)
+    displayConditions.append(currentDayForecast.conditionImg)
+    
+    displayColumn1.append(displayCity)
+    currentDayDisplay.append(displayColumn1)
+    
+    displayColumn1.append(displayDate);
+    displayColumn1.append(displayTemp);
+    displayColumn1.append(displayTemp);
+    displayColumn1.append(displayWind);
+    displayColumn1.append(displayHumidity);
+    displayColumn1.append(displayUvi);
+    displayColumn1.append(displayConditions);
+
+
+}
+
+currentDay()
 
 
 
